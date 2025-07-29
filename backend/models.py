dbce87644bc6cef9b2698b0c4d1f9350b0ebdbb9
JusticeOfEternity <c08015050@gmail.com>
2025-07-29 06:06:15 +0300

    changed SessionID models created_ad property default value

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,8 @@ from sqlmodel import Field, SQLModel, create_engine, Session, select
 from datetime import datetime
 
 class User(SQLModel, table=True):
+    __tablename__ = "User"
+
     id: str | None = Field(default=None, primary_key=True)
     username: str
     mail: str
@@ -13,5 +15,5 @@ class SessionID(SQLModel, table=True):
 
     id: str | None = Field(default=None, primary_key=True)
     session_id: str
-    created_at: int = Field(default=datetime.now().second)
+    created_at: int = Field(default=int(datetime.now().timestamp()))
     expires_at: int
