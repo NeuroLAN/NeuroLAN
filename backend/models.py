a03c0ee8d51cdaaf19b4db2b46128589a3afaebf
JusticeOfEternity <c08015050@gmail.com>
2025-07-29 17:47:17 +0300

    will add not unique display name for adding usernames lower-case to db because of users possibly make same usernames with different upper and lower case letters

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,13 @@
 from sqlmodel import Field, SQLModel, create_engine, Session, select
 from datetime import datetime
+from typing import Optional
 
 class User(SQLModel, table=True):
     __tablename__ = "User"
 
     id: str | None = Field(default=None, primary_key=True)
     username: str
+    display_name: Optional[str] = None
     mail: str
     password: str
 
